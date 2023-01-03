8846f26842976bcb7193b73404644930fc292cf0
unknown <ntttrang.20it2@vku.udn.vn>
2023-01-03 16:49:21 +0700

    code java

---
+++ src/main/java/com/laptopshop/controller/AdminController.java
@@ -0,0 +1,94 @@
+package com.laptopshop.controller;
+
+import com.laptopshop.entities.NguoiDung;
+import com.laptopshop.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/admin")
+@SessionAttributes("loggedInUser")
+public class AdminController {
+
+	@Autowired
+	private DanhMucService danhMucService;
+
+	@Autowired
+	private HangSanXuatService hangSXService;
+
+	@Autowired
+	private NguoiDungService nguoiDungService;
+
+	@Autowired
+	private VaiTroService vaiTroService;
+	
+	@Autowired
+	private LienHeService lienHeService;
+
+	@ModelAttribute("loggedInUser")
+	public NguoiDung loggedInUser() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		return nguoiDungService.findByEmail(auth.getName());
+	}
+
+	@GetMapping
+	public String adminPage(Model model) {
+		return "admin/trangAdmin";
+	}
+
+	@GetMapping("/danh-muc")
+	public String quanLyDanhMucPage() {
+		return "admin/quanLyDanhMuc";
+	}
+
+	@GetMapping("/nhan-hieu")
+	public String quanLyNhanHieuPage() {
+		return "admin/quanLyNhanHieu";
+	}
+
+	@GetMapping("/lien-he")
+	public String quanLyLienHePage() {
+		return "admin/quanLyLienHe";
+	}
+	
+	@GetMapping("/san-pham")
+	public String quanLySanPhamPage(Model model) {
+		model.addAttribute("listNhanHieu", hangSXService.getALlHangSX());
+		model.addAttribute("listDanhMuc", danhMucService.getAllDanhMuc());
+		return "admin/quanLySanPham";
+	}
+
+	@GetMapping("/profile")
+	public String profilePage(Model model, HttpServletRequest request) {
+		model.addAttribute("user", getSessionUser(request));
+		return "admin/profile";
+	}
+
+	@PostMapping("/profile/update")
+	public String updateNguoiDung(@ModelAttribute NguoiDung nd, HttpServletRequest request) {
+		NguoiDung currentUser = getSessionUser(request);
+		currentUser.setDiaChi(nd.getDiaChi());
+		currentUser.setHoTen(nd.getHoTen());
+		currentUser.setSoDienThoai(nd.getSoDienThoai());
+		nguoiDungService.updateUser(currentUser);
+		return "redirect:/admin/profile";
+	}
+
+	@GetMapping("/tai-khoan")
+	public String quanLyTaiKhoanPage(Model model) {
+	    model.addAttribute("listVaiTro", vaiTroService.findAllVaiTro());
+		return "admin/quanLyTaiKhoan";
+	}
+	public NguoiDung getSessionUser(HttpServletRequest request) {
+		return (NguoiDung) request.getSession().getAttribute("loggedInUser");
+	}
+	
+	
+
+}
---
+++ src/main/webapp/WEB-INF/pages/client/thankYou.jsp
@@ -0,0 +1,61 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+	<head>
+		<script src="Frontend/js/jquery.min.js"></script>
+		<script src="Frontend/js/responsiveslides.min.js"></script>
+		<script src="js/client/accounting.js"></script>
+	<title>Thanh toán</title>
+	</head>
+
+	<script type="text/javascript">	  
+	$(document).ready(function() { 
+		 $(".mytable .donGia .check").each(function() {
+			  var value = accounting.formatMoney($(this).text())+ ' VND';
+			  $(this).html(value);
+			});
+	  
+		 $(".mytable .total").each(function() {
+			  var value = accounting.formatMoney($(this).text()) + ' VND ';
+			  $(this).html(value);
+			 
+			});
+	  });
+	</script>
+	<body>
+		
+		<!----start-Header---->
+	 <jsp:include page="include/homeHeader.jsp"></jsp:include>
+		<!----End-Header---->
+		 
+	<div class= "container">
+	<div class="row">
+		<div class="col-md-1">
+		</div>
+		<div class="col-md-10">
+		<br>
+		<p><b>QUÝ KHÁCH ĐÃ ĐẶT HÀNG THÀNH CÔNG</b></p><br>
+
+		<p>LaptopShop xin chân thành cảm ơn quý khách hàng đã tin tưởng sử dụng dịch vụ, sản phẩm của chúng tôi</p><br>
+		<a href="<%=request.getContextPath()%>/">Nhấn vào đây để tiếp tục mua sắm</a>
+		</div>
+		<div class="col-md-1">
+		</div>
+	
+	
+	
+	</div>
+	
+	
+	</div>
+		   
+		<!----start-Footder---->
+	<jsp:include page="include/homeFooter.jsp"></jsp:include>
+	<!----End-Footder---->
+	<script src="<c:url value='/js/client/checkoutAjax.js'/>" ></script>	
+	</body>
+</html>
+
